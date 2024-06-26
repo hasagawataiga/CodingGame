1c0099778517a0ad5ca8f0250491c74dac0767b2
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-06-26 20:32:08 +0200

    New Clash of Code solution

---
+++ Clash_of_code/Bronze/2.java
@@ -0,0 +1,8 @@
+public class 2 {
+    
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(1000 - 2 * n);
+    }
+}
